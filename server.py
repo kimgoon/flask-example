3f3144b7285007cc52d09c59a4247f4150f6219e
kimgoon <kimgoon@github.com>
2017-07-30 17:51:50 -0400

    add charts example, use template to serve charts

--- server.py
+++ server.py
@@ -1,14 +1,28 @@
 from flask import Flask, request, redirect, url_for
+from flask import render_template
+
+import glob, os
 
 # setup flask app
-app = Flask(__name__)
+app = Flask(__name__, template_folder='static')
 
 app.debug = True
 
 
 @app.route('/')
 def root():
-    return app.send_static_file('index.html')
+    chart_name = "Example Charts"
+    charts = []
+
+    for f in os.listdir('static'):
+        if f.endswith('.png'):
+            charts.append(f)
+            print 'appending', f
+
+    #charts.append('figure_1.png')
+    #charts.append('figure_2.png')
+    return render_template('index.html',
+        charts=charts)
 
 
 @app.route('/<path:path>')
