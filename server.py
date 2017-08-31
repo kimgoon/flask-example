e7608f02ec3664d30e97bf28f23a22c9bf1cf3b4
kimgoon <kimgoon@github.com>
2017-08-30 22:13:15 -0400

    adding missed files

--- server.py
+++ server.py
@@ -3,14 +3,46 @@ from flask import render_template
 
 import glob, os
 
+from app import internal
+
 # setup flask app
 app = Flask(__name__, template_folder='static')
 
 app.debug = True
 
 
+data = {
+    'machine1': [
+        {
+            'cpus': 1,
+            'memory': 2,
+        },
+        {
+            'cpus': 10,
+            'memory': 2,
+        },
+          
+    ],
+    'machine2' : [
+        {
+            'cpus': 4,
+            'memory': 6
+        },        {
+            'cpus': 8,
+            'memory': 6
+        }
+    ],
+}
+
+
 @app.route('/')
 def root():
+    print request
+    if request.method == 'GET':
+        print "GET"
+    elif request.method == 'POST':
+        print "POST"
+
     chart_name = "Example Charts"
     charts = []
 
@@ -19,11 +51,16 @@ def root():
             charts.append(f)
             print 'appending', f
 
-    #charts.append('figure_1.png')
-    #charts.append('figure_2.png')
     return render_template('index.html',
         charts=charts)
 
+@app.route('/result', methods=['POST'])
+def result():
+    # print out some debug statements here
+    print "hello"
+    print request.form["value"]
+    internal.work()
+    return render_template('result.html', datas=data)
 
 @app.route('/<path:path>')
 def static_proxy(path):
